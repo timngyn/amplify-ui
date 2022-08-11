2b867f66b3f1145ba65507cd1a06abf136ff8a5e
Ioana Brooks <tryioana@amazon.com>
2022-08-11 15:01:58 -0700

    chore(ui-react-native): Add storybook

--- examples/react-native/index.js
+++ examples/react-native/index.js
@@ -1,5 +1,10 @@
 import { AppRegistry } from 'react-native';
 import App from './App';
+import StorybookUIRoot from './storybook';
 import { name as appName } from './app.json';
 
-AppRegistry.registerComponent(appName, () => App);
+const isStorybook = true;
+
+AppRegistry.registerComponent(appName, () =>
+  isStorybook ? StorybookUIRoot : App
+);
--- examples/react-native/storybook/index.tsx
+++ examples/react-native/storybook/index.tsx
@@ -1,6 +1,3 @@
-// if you use expo remove this line
-import { AppRegistry } from 'react-native';
-
 import {
   getStorybookUI,
   configure,
@@ -21,9 +18,5 @@ configure(() => {
 // Refer to https://github.com/storybookjs/react-native/tree/master/app/react-native#getstorybookui-options
 // To find allowed options for getStorybookUI
 const StorybookUIRoot = getStorybookUI({});
-
-// If you are using React Native vanilla and after installation you don't see your app name here, write it manually.
-// If you use Expo you should remove this line.
-AppRegistry.registerComponent('ReactNative', () => StorybookUIRoot);
 
 export default StorybookUIRoot;
